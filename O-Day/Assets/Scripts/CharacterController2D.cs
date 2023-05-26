9bb25039420ffc79c8d92719c8d64aad69eef96b
zoeeechu <43968711+zoeeechu@users.noreply.github.com>
2023-05-26 02:35:38 -0400

    [[bug fix]] Sticking to Wall
    Fixed bug player sticking to wall
    replaced old method of moving player with rigidbody 2D
    
    IMPORTANT the shooting logic is commented out and the jumping needs to be tuned to feel like before

--- O-Day/Assets/Scripts/CharacterController2D.cs
+++ O-Day/Assets/Scripts/CharacterController2D.cs
@@ -15,17 +15,45 @@ public class CharacterController2D : MonoBehaviour
     [SerializeField, Tooltip("Deceleration applied when character is grounded and not attempting to move.")]
     float groundDeceleration = 90;
 
+/*
     [SerializeField, Tooltip("Max height the character will jump regardless of gravity")]
     float jumpHeight = 3;
-
+*/
     [SerializeField, Tooltip("Prefab for bullet")]
     GameObject playerBulletPrefab;
+
+    // zoe - ///////////////////////////////////////////
+
+    [SerializeField, Tooltip("Max height the character will jump regardless of gravity")]
+    public float jumpforce;
+
+    private float moveInput;
+    private Rigidbody2D rb;
+    private int extraJumps;
+
+    [SerializeField, Tooltip("Any extra amount of jumps?")]
+    public int extraJumpValue;
+
+    private bool isGrounded;
+
+    [SerializeField, Tooltip("Object To check if Player is On ground")]
+    public Transform groundCheck;
+
+    [SerializeField, Tooltip("How far does the Object check (Recommend 0.5) ")]
+    public float checkRadius;
+
+    [SerializeField, Tooltip("Which Tags are considered Ground")]
+    public LayerMask whatIsGround;
+
+    private bool facingRight = true;
+    //////////////////////////////////////////////////
+    
 
     private BoxCollider2D boxCollider;
 
     private Vector2 velocity;
 
-    private bool grounded;
+    // removed Ground -zoe
 
     public float fireRate = 0.2f;
 
@@ -35,10 +63,65 @@ public class CharacterController2D : MonoBehaviour
     private void Awake()
     {      
         boxCollider = GetComponent<BoxCollider2D>();
+
+        extraJumps = extraJumpValue;
+
+        // added Rigidbody2D as rb for more ctrl? -zoe
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
-    private void Update()
+// chnaged from Update to FixedUpdate -zoe 
+    private void FixedUpdate()
     {   
+
+         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+
+        
+        //use GetAxisRaw for more Snappy movement if desired -zoe
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+
+        // make charecter Sprite face the direction its moving... 
+        // if we have seprate sprites for rigth and left howver we can remove this -zoe
+        if(facingRight == false && moveInput > 0){
+            Flip();
+        }else if(facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+        
+       
+        }
+
+        void Update(){
+
+
+
+        // new jump function -zoe /////
+
+          if (isGrounded == true)
+        {
+            extraJumps = extraJumpValue;
+           
+        }
+    
+          
+            if(Input.GetButtonDown("Jump") && extraJumps > 0){
+
+                rb.velocity = Vector2.up * jumpforce;
+                extraJumps--;
+            } else if(Input.GetButtonDown("Jump") && extraJumps == 0 && isGrounded == true)
+            {
+                rb.velocity = Vector2.up * jumpforce;
+            }
+
+         /////////////////////////
+
+
+          /*
         if (grounded)
         {
 	        velocity.y = 0;
@@ -110,6 +193,20 @@ public class CharacterController2D : MonoBehaviour
                 if (Vector2.Angle(colliderDistance.normal, Vector2.up) < 90 && velocity.y < 0){
                     grounded = true;
                 }
-            }
+            }*/
+
         }
+
+        
+        // Flips charecter Sprite to opposite of whatever there facing -zoe
+        void Flip(){
+            facingRight = !facingRight;
+            Vector3 Scaler = transform.localScale;
+            Scaler.x *= -1;
+            transform.localScale = Scaler;
+        }
+
+
+
+
 }
