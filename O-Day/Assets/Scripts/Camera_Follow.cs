39926d56500e94f88705c6d813058706165e86cd
zoeeechu <43968711+zoeeechu@users.noreply.github.com>
2023-05-28 22:21:13 -0400

    Added CameraFollow
    Camera now follows player smoothly.

---
+++ O-Day/Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    //Camera Follow Player Script.
+    [SerializeField, Tooltip("What you want the Camera to Follow")]
+    public Transform target;
+
+    [SerializeField, Tooltip("Edit Camera Smoothness Default (0.125f)")]
+    public float damping = 0.125f;
+
+    [SerializeField, Tooltip("Offset Camera")]
+    public Vector3 offset;
+
+    private Vector3 velocity = Vector3.zero;
+
+    void FixedUpdate (){
+        Vector3 desiredPos = target.position + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPos, ref velocity, damping);
+
+
+    }
+}
