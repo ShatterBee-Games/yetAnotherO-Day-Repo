825feb8bc2df9061cece2d8dd6f9f49cf86fb9b5
zoeeechu <43968711+zoeeechu@users.noreply.github.com>
2023-05-30 00:20:16 -0400

    PauseMenu Cleanup
    Cleaned up my code

--- O-Day/Assets/Scripts/PauseMenu.cs
+++ O-Day/Assets/Scripts/PauseMenu.cs
@@ -12,66 +12,60 @@ public class PauseMenu : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
 
-        // removed if Stament and replaced it with one funtion :> -zoe
-            pause();
-
-            /*
-            ////// REMOVED //
-            if (pauseDisplay)
-            {
-                pauseOff();
-            } else
-            {
-                pauseOn();
-            }
-            ///////////////
-            */
+            // removed if Stament and replaced it with one funtion :> -zoe
+            pauseDisplay = !pauseDisplay;
+            pauseUI.SetActive(pauseDisplay);
+            float time = pauseDisplay ? 0f : 1f;
+            Time.timeScale = time;
 
         }
     }
 
 
-// Replaced 2 functions with 1 -zoe
+    /*
 
- public void pause()
- {
-    pauseDisplay = !pauseDisplay;
-    pauseUI.SetActive(pauseDisplay);
-    float time = pauseDisplay ? 0f : 1f;
-    Time.timeScale = time;
- }
+    Hey Shane! heres a little explination! 
 
-/*
+    pauseDisplay = !pauseDisplay; - sets the bool to oppostie of itself
 
-Hey Shane! heres a little explination! 
+    float time = pauseDisplay ? 0f : 1f; - creates a float named time and makes it work with bool pauseDisplay 
+    if pauseDisplay is true then 1f if false then 0f 
 
-pauseDisplay = !pauseDisplay; - sets the bool to oppostie of itself
+    Time.timeScale = time; - then we just connect time to Time.timeScale
 
-float time = pauseDisplay ? 0f : 1f; - creates a float named time and makes it work with bool pauseDisplay 
-if pauseDisplay is true then 1f if false then 0f 
-
-Time.timeScale = time; - then we just connect time to Time.timeScale
-
-hope this helps zoe <3 
+    hope this helps zoe <3 
 
 
-///////////// REMOVED ///////////////////////////
+    ///////////// REMOVED ///////////////////////////
 
-    //Feel free to edit this code and leave behind a comment of what I should've done instead (Shane)
-    public void pauseOn()
-    {
-        pauseUI.SetActive(true);
-        pauseDisplay = true;
-        Time.timeScale = 0f;
+     if (Input.GetKeyDown(KeyCode.Escape)){
+
+        if (pauseDisplay)
+        {
+                pauseOff();
+        } else
+        {
+                pauseOn();
+        }
+
     }
-    public void pauseOff()
-    {
-        pauseUI.SetActive(false);
-        pauseDisplay = false;
-        Time.timeScale = 1f;
     }
-///////////////////////////////////////////////////
-*/
+
+        //Feel free to edit this code and leave behind a comment of what I should've done instead (Shane)
+        public void pauseOn()
+        {
+            pauseUI.SetActive(true);
+            pauseDisplay = true;
+            Time.timeScale = 0f;
+        }
+        public void pauseOff()
+        {
+            pauseUI.SetActive(false);
+            pauseDisplay = false;
+            Time.timeScale = 1f;
+        }
+    ///////////////////////////////////////////////////
+    */
 
 
 }
